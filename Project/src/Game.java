69eed1b318ae44ef72a89e0c7719096832290aab
Will Borland <wborland@purdue.edu>
2016-11-28 22:38:31 -0500

    Start

---
+++ Project/src/Game.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Game {
+
+
+    ArrayList<String> gameTokenList = new ArrayList<String>();                //ArrayList to store all of the game keys
+    HashMap<String,ArrayList<Player>> gameKeyMap = new HashMap<String,ArrayList<Player>>(); //Map to hold a list of
+    // players for each game
+
+}
