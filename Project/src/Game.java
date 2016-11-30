c01aa332a5f64989ae4538c8cce46161688f5ec7
Will Borland <wborland@purdue.edu>
2016-11-29 20:51:36 -0500

    Communicatino

--- Project/src/Game.java
+++ Project/src/Game.java
@@ -1,10 +1,112 @@
+
 import java.util.*;
+import java.io.*;
+import java.net.*;
 
-public class Game {
+public class Game implements Runnable{
+    Socket socket;
 
-
-    ArrayList<String> gameTokenList = new ArrayList<String>();                //ArrayList to store all of the game keys
-    HashMap<String,ArrayList<Player>> gameKeyMap = new HashMap<String,ArrayList<Player>>(); //Map to hold a list of
+    public static ArrayList<String> gameTokenList = new ArrayList<String>();     //ArrayList to store all of the game keys
+    public static HashMap<String,ArrayList<Player>> gameKeyMap = new HashMap<String,ArrayList<Player>>(); //Map to hold a list of
     // players for each game
+
+    public Game(Socket socket){
+        this.socket = socket;
+    }
+
+
+    public static void main(String[] args) throws IOException{
+
+        ServerSocket serverSocket = new ServerSocket(50000);
+        System.out.println("Waiting for connection");
+        while(true) {
+            Socket socket = serverSocket.accept();
+            Game game = new Game(socket);
+            new Thread(game).start();
+        }
+
+    }
+
+
+
+
+    public void run() {
+        System.out.println("Connection received from " + socket.getLocalSocketAddress());
+
+        try {
+
+            PrintWriter printWriter = new PrintWriter(socket.getOutputStream());
+            Scanner scan = new Scanner(socket.getInputStream());
+
+
+
+            while (scan.hasNextLine()) {
+                String input = scan.nextLine();
+                System.out.println("Recived from client: " + input);
+
+                printWriter.printf("Echo: %s\n", input);
+                printWriter.flush();
+            }
+
+
+            printWriter.close();
+            scan.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String parse(String input){
+        String output = "";
+
+        if(input.contains("CREATENEWUSER")){
+            output = newUser(input);
+        }else if(input.contains("LOGIN")){
+            output = userLogin(input);
+        }
+
+        return output;
+    }
+
+    public String newUser(String input){
+
+        return null;
+    }
+
+    public String userLogin(String input){
+
+        return null;
+    }
+
+    public String newGame(String input){
+
+        return null;
+    }
+
+    public String joinGame(String input){
+
+        return null;
+    }
+
+    public String sendWord(String input){
+
+        return null;
+    }
+
+    public String launchGame(String input){
+
+        return null;
+    }
+
+    public String suggestions(String input){
+
+        return null;
+    }
+
+    public
+
+
+
+
 
 }
